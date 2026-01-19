ef62f2aca303da3b7ca1351cdd58da92ef1cc53d
Hugo Sauvage <124774769+hugosk8@users.noreply.github.com>
2026-01-19 16:40:13 +0100

    "Imports" section done

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ test_labels = "input/t10k-labels.idx1-ubyte"
 loader = MnistDataLoader(train_imgs, train_labels, test_imgs, test_labels)
 loader.load_data()
 
-loader.display_basic_stats("train")
-loader.display_basic_stats("test")
+# Show digits distribution
+# loader.display_basic_stats("train")
+# loader.display_basic_stats("test")
+
+# Show selected image
 # loader.show_image(0, "train")
 # loader.show_image(42, "test")
-loader.show_mean_digits("train")
 
-loader.flatten_images_inplace(normalize=True)
-print(loader.X_train_flat.shape)
-print(loader.X_test_flat.shape) 
+# # Show mean digits
+# loader.show_mean_digits("train")
+
+# # Show flatten images size
+# loader.flatten_images_inplace(normalize=True)
+# print(loader.X_train_flat.shape)
+# print(loader.X_test_flat.shape) 
+
+print(loader.display_digits_distribution_percent_graph())
